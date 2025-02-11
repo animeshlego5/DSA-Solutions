6cf03a7c7b2742a1e4b78493bb7b19c67b27a0e4
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2025-02-12 00:09:08 +0530

    Time: 1 ms (100%), Space: 44.7 MB (49.23%) - LeetHub

--- 3234-double-modular-exponentiation/3234-double-modular-exponentiation.java
+++ 3234-double-modular-exponentiation/3234-double-modular-exponentiation.java
@@ -1,17 +1,32 @@
 class Solution {
     public List<Integer> getGoodIndices(int[][] variables, int target) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        for(int i = 0;i<variables.length;i++){
-            int a =variables[i][0];
+        for (int i = 0; i < variables.length; i++) {
+            int a = variables[i][0];
             int b = variables[i][1];
             int c = variables[i][2];
             int m = variables[i][3];
-            int power = (int)Math.pow(a,b)%10;
-            int bracket1 = (int)Math.pow(power,c);
-            int ans = bracket1 %m;
-            if(target==ans){
+            int power = (int) customPow(a,b,10);
+            int ans = (int) customPow(power,c,m);
+            
+            if (target == ans) {
                 result.add(i);
             }
         }
-    return result;}
+        return result;
+    }
+
+    public long customPow(int base, int exponent, int modulus) {
+        long answer = 1;
+        base = base%modulus;
+        while(exponent>0){
+            if(exponent%2!=0){
+                exponent -=1;
+                answer = (answer * base)%modulus;
+            }
+            exponent /=2;
+            base =(base*base)%modulus;
+        }
+        return answer;
+    }
 }
