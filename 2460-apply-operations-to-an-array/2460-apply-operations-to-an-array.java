34aea5f1e63abdb43fa12c5b71287f6a6dc5e168
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2025-02-12 17:19:22 +0530

    Time: 1 ms (96.13%), Space: 43.8 MB (57.55%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n - 1; i++) {
-            if(nums[i] == nums[i + 1]) {
-                nums[i] *= 2;
-                nums[i + 1] = 0;
-                i++;
+        for(int i = 0;i+1<nums.length;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
             }
         }
-        int[] res = new int[n];
-        int k = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] != 0)
-                res[k++] = nums[i];
+        reformat(nums);
+        return nums;
+    }
+    public void reformat(int[] nums){
+        for(int i = 0,idx=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                swap(nums,i,idx);
+                idx++;
+            }
         }
-        return res;
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
