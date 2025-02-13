5837ac4dfe1b7790d29dc87a5c9f23a913453e3b
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2025-02-13 23:32:30 +0530

    Time: 24 ms (5.73%), Space: 58.6 MB (25.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for(int ele : nums){
-            if(seen.contains(ele)){
-                return true;
-            }
-            seen.add(ele);
-        }
-        return false;
+        return Arrays.stream(nums).distinct().count()<nums.length;
     }
 }
