18c72dfc37892a13df20dac4dc62fdabdeb6d014
animeshlego5 <126562373+animeshlego5@users.noreply.github.com>
2025-02-25 23:30:15 +0530

    Time: 10 ms (95.63%), Space: 49.6 MB (7.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,28 @@
-public class Solution {
+class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        List<Integer>[] freq = new List[nums.length + 1];
-
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
+        Map <Integer, Integer> count = new HashMap<>();
+        for(int i : nums){
+            count.put(i,(count.getOrDefault(i,0)+1));
         }
-
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
+        List<Integer> [] res = new List[nums.length+1];
+        for(int i = 0;i<res.length;i++){
+            res[i] = new ArrayList<>();
         }
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            freq[entry.getValue()].add(entry.getKey());
+        for(Map.Entry<Integer, Integer> entry:count.entrySet()){
+            res[entry.getValue()].add(entry.getKey());
         }
-
-        int[] res = new int[k];
+        int[] result = new int[k];
         int index = 0;
-        for (int i = freq.length - 1; i > 0 && index < k; i--) {
-            for (int n : freq[i]) {
-                res[index++] = n;
-                if (index == k) {
-                    return res;
-                }
+        for(int i = res.length-1;i>=0&&index<k;i--){
+            for(int n :res[i]){
+            result[index++]=n;
+            if(index==k){
+                return result;
+            }
             }
         }
-        return res;
+
+        return result;
     }
+    
 }
