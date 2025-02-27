e4f34034ab6e9dd2ea43e01c94eab7e9000de24a
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-02-27 12:28:18 +0530

    Time: 13 ms (24.02%), Space: 44.8 MB (33.8%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,18 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> seen = new HashSet<>();
-        for(int i = 0;i<9;i++){
-            for(int j = 0;j<9;j++){
-                if(board[i][j]=='.'){
-                    continue;
+        
+        // Iterate over each cell in the 9x9 board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;  // Skip empty cells
                 }
-            if(!seen.add(board[i][j] + "seen in row" + i) ||
-               !seen.add(board[i][j] + "seen in column" + j) ||
-               !seen.add(board[i][j]+"seen in box" + i/3 + "-" + j/3)){
-                return false;
-               }
+                
+                // Store the current number as an integer
+                int num = board[i][j];
+                
+                // Check and add to the set for row, column, and box
+                if (!seen.add(num + " seen in row " + i) ||
+                    !seen.add(num + " seen in column " + j) ||
+                    !seen.add(num + " seen in box " + (i / 3) + "-" + (j / 3))) {
+                    return false;  // Duplicate found
+                }
             }
         }
-        return true;
+        
+        return true;  // No duplicates found, valid Sudoku
     }
 }
