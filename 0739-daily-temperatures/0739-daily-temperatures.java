61a598ff0036ecb863c3ac537e52003fe4e437fb
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-03-04 23:10:33 +0530

    Time: 61 ms (78.48%), Space: 63.2 MB (19.8%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
         Stack <Integer> stack = new Stack<>();
-        int k = 0;
         for(int i = 0;i<temperatures.length;i++){
             while(stack.size()!=0 && temperatures[stack.peek()]<temperatures[i]){
                 int pop = stack.pop();
