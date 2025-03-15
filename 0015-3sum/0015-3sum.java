2d54b85aebd13b71ce970df83766fd1777dbf9d9
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-03-15 22:41:57 +0530

    Time: 29 ms (72.25%), Space: 51.4 MB (71.65%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,41 +2,44 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         ArrayList<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
+        
         for (int i = 0; i < nums.length; i++) {
-
+            // Skip duplicate values for i
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
-            int l = i+1;
-            int r = (i == nums.length - 1) ? nums.length - 2 : nums.length - 1;
-
+            
+            int l = i + 1;
+            int r = nums.length - 1;
+            
             while (l < r) {
-                if (l == i) {
+                int sum = nums[i] + nums[l] + nums[r];
+                
+                if (sum < 0) {
                     l++;
-                }
-                if (r == i) {
-                    r--;
-                }
-
-                if (nums[l] + nums[r] + nums[i] < 0) {
-                    l++;
-                } else if (nums[l] + nums[r] + nums[i] > 0) {
+                } else if (sum > 0) {
                     r--;
                 } else {
-
+                    // Found a valid triplet
                     list.add(List.of(nums[i], nums[l], nums[r]));
+                    
+                    // Skip duplicates for l
                     while (l < r && nums[l] == nums[l + 1]) {
                         l++;
                     }
+                    
+                    // Skip duplicates for r
                     while (l < r && nums[r] == nums[r - 1]) {
                         r--;
                     }
+                    
+                    // Move both pointers
                     l++;
                     r--;
                 }
             }
         }
+        
         return list;
     }
 }
