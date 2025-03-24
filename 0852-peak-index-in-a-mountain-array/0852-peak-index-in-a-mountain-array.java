ed66c357582f1e34075aec41369d945ac298d715
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-03-24 18:38:44 +0530

    Time: 0 ms (100%), Space: 56.4 MB (34.51%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,12 +1,18 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i = 2;
-        while(i<arr.length){
-            if(arr[i-2]<arr[i-1] && arr[i-1]>arr[i]){
-                return i-1;
+        int start = 0;
+        int end = arr.length - 2;
+        int mid = 0;
+        while (start<=end) {
+            mid = end + (start - end)/2;
+            if(arr[mid -1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
+            } else if(arr[mid]<arr[mid+1]){
+                start = mid + 1;
+            } else if(arr[mid]<arr[mid-1]){
+                end = mid - 1;
             }
-            i++;
         }
-        return i;
+        return mid;
     }
 }
