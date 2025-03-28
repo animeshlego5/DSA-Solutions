cb20949302f3532acaa795087f231850475bcfc9
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-03-28 23:36:57 +0530

    Time: 2 ms (58.09%), Space: 46.2 MB (43.34%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,24 @@
-class Solution {
+public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
-            }
-
+        List<Integer> merged = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
+                merged.add(nums1[i++]);
             } else {
-                new_arr[k++] = nums2[j++];
+                merged.add(nums2[j++]);
             }
         }
-
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
+        
+        while (i < nums1.length) merged.add(nums1[i++]);
+        while (j < nums2.length) merged.add(nums2[j++]);
+        
+        int mid = merged.size() / 2;
+        if (merged.size() % 2 == 0) {
+            return (merged.get(mid-1) + merged.get(mid)) / 2.0;
+        } else {
+            return merged.get(mid);
+        }
     }
 }
