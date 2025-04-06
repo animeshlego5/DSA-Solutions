5bd013fc2fd33570ce485d1a956fde9afc14356a
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-04-06 21:17:13 +0530

    Time: 0 ms (100%), Space: 50.5 MB (30.12%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-arraypush.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 2;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == nums[mid^1]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
+}
