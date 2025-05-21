7bb993cd8d1369351f1dc8b8cf92f29cf3a667ea
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-05-21 16:51:58 +0530

    Time: 2 ms (61.42%), Space: 44.6 MB (42.47%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer,Integer> index = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            index.put(inorder[i],i);
+        }
+        int n = preorder.length - 1;
+        int m = inorder.length - 1;
+        TreeNode root = constructTree(preorder, inorder, index, 0,n, 0, m);
+        return root;
+
+    }
+    public TreeNode constructTree(int[] preorder, int[] inorder, HashMap<Integer,Integer> index, int preIndStart, int preIndEnd, int inIndStart, int inIndEnd){
+        //base case
+        if(preIndStart>preIndEnd || inIndStart > inIndEnd){
+            return null;
+        }
+        int rootValue = preorder[preIndStart];
+        int rootIndex = index.get(rootValue);
+        TreeNode root = new TreeNode(rootValue);
+        int leftSubTree = rootIndex - inIndStart;
+        int rightSubTree = inIndEnd - rootIndex;
+        root.left = constructTree(preorder, inorder, index,preIndStart +1 ,preIndStart + leftSubTree, inIndStart, rootIndex-1);
+        root.right = constructTree(preorder, inorder, index, preIndStart + leftSubTree + 1,preIndEnd, rootIndex+1, inIndEnd);
+
+    return root;}
+}
