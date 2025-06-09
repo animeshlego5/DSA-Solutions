ac288a435a4a5aaa1b6c6841a5f909b40a7f6832
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-06-09 13:33:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (58.36%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,35 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        
+
         List<List<Integer>> list = new ArrayList<>();
-        if(root==null)return list;
+        if (root == null)
+            return list;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         int count = 1;
-        while(!queue.isEmpty()){
-            
+        while (!queue.isEmpty()) {
+
             int size = queue.size();
             List<Integer> sublist = new LinkedList<>();
-            
-            for(int i = 0; i<size; i++){
-                TreeNode node = queue.poll();
-                if(count%2==0){
-                    sublist.addFirst(node.val);
-                } else {sublist.add(node.val);}
 
-                if(node.left!=null){
-                        queue.offer(node.left);
-                    } 
-                    if(node.right!=null){
-                        queue.offer(node.right);
-                    }
-                
-                
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (count % 2 == 0) {
+                    sublist.addFirst(node.val);
+                } else {
+                    sublist.add(node.val);
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
             }
             count++;
             list.add(sublist);
         }
-    return list;
+        return list;
     }
 }
