97a02eb456653e94e43316c581a9e6b4afbf0aee
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-01 23:24:01 +0530

    Time: 7 ms (63.24%), Space: 43.1 MB (13.23%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -1,41 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     List<String> res = new ArrayList<>();
-
+    
     public List<String> binaryTreePaths(TreeNode root) {
-        if(root==null)return res;
-        StringBuilder s = new StringBuilder();
-        dfs(root, s);
-return res;
+        if (root == null) return res;
+        dfs(root, "");
+        return res;
     }
-    public void dfs(TreeNode root, StringBuilder s){
-        if(root==null)return;
-        int len = s.length();
-        if(len==0){
-                s.append(root.val);
-            } else {
-                s.append("->" + root.val);
-            }
-        if(root.left==null && root.right==null){
-            res.add(s.toString());
+
+    public void dfs(TreeNode root, String path) {
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) {
+            res.add(path + root.val);
         } else {
-            dfs(root.left, s);
-            dfs(root.right, s);
+            path += root.val + "->";
+            dfs(root.left, path);
+            dfs(root.right, path);
         }
-    s.setLength(len);
     }
 }
