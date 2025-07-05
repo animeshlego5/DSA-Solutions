08913cdd883895c7833d1057963bea34337379a0
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-05 08:14:59 +0530

    Time: 1 ms (69.99%), Space: 40.9 MB (67.35%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,15 +1,14 @@
 class Solution {
-    Map<Integer, Integer> dp = new HashMap<>();
     public int combinationSum4(int[] nums, int target) {
-        if(target==0) return 1;
-        if(dp.containsKey(target))return dp.get(target);
-        int count = 0;
-        for(int num:nums){
-            if(num<=target){
-                count+=combinationSum4(nums, target-num);
+        int[] dp = new int[1+target];
+        dp[0]=1;
+        for(int i = 1; i <=target; i++){
+            for(int num:nums){
+                if(i-num>=0){
+                    dp[i] += dp[i-num];
+                }
             }
         }
-        dp.put(target, count);
-        return count;
+        return dp[target];
     }
 }
