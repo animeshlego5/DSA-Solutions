8f4f27c0d4761a17a9581e5014ed9a1b6c365fd6
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-19 14:16:07 +0530

    Time: 36 ms (75.22%), Space: 59.1 MB (46.39%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,11 +1,26 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int num: nums){
-            pq.offer(num);
-            if(pq.size()>k)pq.poll();
-        
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums){
+            list.add(num);
         }
-        return pq.poll();
+        return quickSelect(list, k);
+    }
+    public int quickSelect(List<Integer>list, int k){
+        if(list.size()==1)return list.get(0);
+        Random rand = new Random();
+        int pivotIndex = rand.nextInt(list.size());
+        int pivot = list.get(pivotIndex);
+        List<Integer> greater = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> lesser = new ArrayList<>();
+        for(int num: list){
+            if(num>pivot)greater.add(num);
+            else if(num==pivot)equal.add(num);
+            else lesser.add(num);
+        }
+        if(greater.size()>=k)return quickSelect(greater, k);
+        if(greater.size()+equal.size()<k)return quickSelect(lesser, k-(greater.size()+equal.size()));
+        return pivot;
     }
 }
