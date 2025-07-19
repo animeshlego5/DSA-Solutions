bedd4f90abb9b16131ec39261ac316a9e10ea13a
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-19 14:20:26 +0530

    Time: 2200 ms (5.01%), Space: 62.1 MB (8.02%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : nums){
-            list.add(num);
-        }
-        return quickSelect(list, k);
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
-    public int quickSelect(List<Integer>list, int k){
-        if(list.size()==1)return list.get(0);
-        Random rand = new Random();
-        int pivotIndex = rand.nextInt(list.size());
-        int pivot = list.get(pivotIndex);
-        List<Integer> greater = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> lesser = new ArrayList<>();
-        for(int num: list){
-            if(num>pivot)greater.add(num);
-            else if(num==pivot)equal.add(num);
-            else lesser.add(num);
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        int pivot = nums[right];
+        int p = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] > pivot) {
+                swap(nums, i, p++);
+            }
         }
-        if(greater.size()>=k)return quickSelect(greater, k);
-        if(greater.size()+equal.size()<k)return quickSelect(lesser, k-(greater.size()+equal.size()));
-        return pivot;
+        swap(nums, p, right);
+        
+        int count = p - left + 1; // Number of elements greater than pivot
+        if (count == k) return nums[p];
+        else if (count > k) return quickSelect(nums, left, p - 1, k);
+        else return quickSelect(nums, p + 1, right, k - count);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
