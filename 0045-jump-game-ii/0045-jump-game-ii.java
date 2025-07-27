48a15dbf38ad2e5a136a5da0186415e8b13a36ec
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-27 23:41:31 +0530

    Time: 1 ms (99.42%), Space: 45.4 MB (40.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length==1)return 0;
+        int lastindex = 0;
+        int jumps = 0;
+        int coverage = 0;
+        for(int i = 0; i < nums.length; i++){
+            coverage = Math.max(coverage, nums[i] + i);
+            if(i==lastindex){
+                jumps++;
+                lastindex = coverage; 
+                 if(coverage>=nums.length-1)return jumps;
+            }
+           
+        }
+        return jumps;
+
+    }
+}
