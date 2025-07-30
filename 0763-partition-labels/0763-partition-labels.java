07fa58c9a2eabff376c852699af4f57721619a6b
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-07-30 20:50:38 +0530

    Time: 9 ms (37.31%), Space: 42.6 MB (36.56%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,20 +1,21 @@
-class Solution {
+public class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> partition = new ArrayList<Integer>();
-        int i = 0;
-        while(i<s.length()){
-            int start = i;
-            int end = s.lastIndexOf(s.charAt(start));
-            for(int j = start + 1; j < end; j++){
-                char c = s.charAt(j);
-                if(s.lastIndexOf(c)<=end){
-                    continue;
-                }
-                end = s.lastIndexOf(c);
-            }
-            partition.add(end-start + 1);
-            i = end + 1;
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex.put(s.charAt(i), i);
         }
-        return partition;
+        
+        List<Integer> res = new ArrayList<>();
+        int size = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, lastIndex.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(size);
+                size = 0;
+            }
+        }
+        return res;
     }
 }
