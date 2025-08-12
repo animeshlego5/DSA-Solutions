2226834ad9a06ff8bf26d7f03c46c992fb5f19f9
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-12 19:59:56 +0530

    Time: 17 ms (73.2%), Space: 47.4 MB (94.4%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,68 @@
+class MyHashMap {
+    private final int SIZE = 769;
+    List<Entry>[] map;
+
+    public MyHashMap() {
+        map = new LinkedList[SIZE];
+    }
+    
+    public void put(int key, int value) {
+        int bucket = key % SIZE;
+        if(map[bucket]==null){
+            map[bucket] = new LinkedList<Entry>();
+            map[bucket].add(new Entry(key, value));
+        } else {
+            for(Entry entry : map[bucket]){
+                    if(entry.key == key){
+                        entry.value = value;
+                        return;
+                    } 
+            }
+            map[bucket].add(new Entry(key, value));
+        }
+    }
+    
+    public int get(int key) {
+        int bucket = key % SIZE;
+        List<Entry> entries = map[bucket];
+        if(entries==null)return -1;
+        for(Entry entry : entries){
+            if(entry.key==key) return entry.value;
+        } 
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int bucket = key % SIZE;
+        Entry toRemove = null;
+        if(map[bucket] == null)return;
+        else {
+            for(Entry entry : map[bucket]){
+                if(entry.key == key){
+                    toRemove = entry;
+                }
+            }
+            if(toRemove==null)return;
+            else map[bucket].remove(toRemove);
+        }
+
+    }
+    class Entry {
+    public int key;
+    public int value;
+    public Entry(int key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
+}
+
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
