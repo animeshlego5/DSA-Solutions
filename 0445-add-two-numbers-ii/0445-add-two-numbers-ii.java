856a8afead870140e19e36b0ca3e99c74a0ca20c
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-14 23:13:41 +0530

    Time: 2 ms (61.8%), Space: 44.6 MB (44.25%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -9,32 +9,40 @@
  * }
  */
 class Solution {
+    public ListNode reverseLL(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode tempNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tempNode;
+        }
+        return prev;
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        while(l1!=null){
-            s1.add(l1.val);
-            l1 = l1.next;
-        }
-        while(l2!=null){
-            s2.add(l2.val);
-            l2 = l2.next;
-        }
+        ListNode dummy = new ListNode (-1);
+        ListNode temp = dummy;
+
+        ListNode p1 = reverseLL(l1);
+        ListNode p2 = reverseLL(l2);
+
         int carry = 0;
-        ListNode head = null;
-        while(!s1.isEmpty() || !s2.isEmpty() || carry!=0){
-            int sum = carry;
-            if(!s1.isEmpty())sum += s1.pop();
-            if(!s2.isEmpty())sum += s2.pop();
-            
-            carry = sum / 10;
-            sum = sum % 10;
-            
-            
-            ListNode temp = new ListNode(sum);
-            temp.next = head;
-            head = temp;
+
+        while(p1 != null || p2 != null || carry != 0){
+            int x = (p1 != null) ? p1.val : 0;
+            int y = (p2 != null) ? p2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum/10;
+
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+
+            if(p1 != null) p1 = p1.next ; 
+            if(p2 != null) p2 = p2.next ;
         }
-        return head;
+        return reverseLL(dummy.next);
     }
 }
