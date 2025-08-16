71831c22996af0992129bedb5cf303af0f521cec
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-16 15:55:18 +0530

    Time: 0 ms (100%), Space: 45.1 MB (93.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int left = 0;
-        int right = nums.length -1;
-        while(left<=right){
-            int middle = right - (right - left)/2;
-            if(nums[middle]==middle){
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
+        int n = nums.length;
+        int sum2 = 0;
+        for (int n1 : nums) {
+            sum2 += n1;
         }
-        return right + 1;
+        int sum1 = n * (n + 1) / 2;
+
+    return sum1- sum2;   
     }
 }
