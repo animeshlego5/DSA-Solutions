87c591a9054977ad3adcfa1d01bb7fef1f6ec5cd
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-19 15:09:16 +0530

    Time: 2 ms (76.59%), Space: 41.5 MB (18.05%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -3,12 +3,12 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         outer:
         for(int i = left; i <= right; i++){
-            if(i<10){res.add(i); continue;}
+            
             int num = i;
             while(num>0){
                 int digit = num%10;
-                if(digit==0)continue outer;
-                if(i%digit!=0)continue outer;
+                
+                if(digit==0 || i%digit!=0)continue outer;
                 num = num/10;
             }
             res.add(i);
