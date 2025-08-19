d86b9ad450c1484ca51ede03802b5e608def0f15
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-19 21:31:40 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,30 @@
 class Solution {
-    public int fib(int n) {
-        if(n<=1)return n;
-        return fib(n-1) + fib(n-2);
+    public static int[] dp = new int[31];
+    static {
+        Arrays.fill(dp, -1); 
+    }
+
+    public  int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        // Temporary variables to store values of fib(n-1) & fib(n-2)
+        int first, second;
+
+        if (dp[n - 1] != -1) {
+            first = dp[n - 1];
+        } else {
+            first = fib(n - 1);
+        }
+
+        if (dp[n - 2] != -1) {
+            second = dp[n - 2];
+        } else {
+            second = fib(n - 2);
+        }
+
+        // Memoization
+        return dp[n] = first + second;
     }
 }
