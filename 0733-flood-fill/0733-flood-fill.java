a7e23ae75415116878a0b48785d4abd854f3e286
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-20 21:05:13 +0530

    Time: 1 ms (44.3%), Space: 45 MB (68.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -9,12 +9,13 @@ class Solution {
         return image;
     }
     private void dfs(int[][]image, int row, int col, int original, int color){
-        
+        int n=image.length;
+        int m=image[0].length;
         image[row][col] = color;
         for(int i = 0; i < 4; i++){
             int newRow = row + delrow[i];
             int newCol = col +delcol[i];
-            if(newCol>=0 && newRow>=0 && newCol<image[0].length &&  newRow<image.length && image[newRow][newCol]==original){
+            if(newCol>=0 && newRow>=0 && newCol<m &&  newRow<n && image[newRow][newCol]==original){
                 dfs(image, newRow, newCol, original, color);
             }
         }
