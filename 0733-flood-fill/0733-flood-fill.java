df5bf3565a846fd4f66a33ce25ee42712cd25cb2
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-20 21:03:43 +0530

    Time: 1 ms (44.3%), Space: 45 MB (68.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,5 +1,6 @@
 class Solution {
-    private int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int delrow[]={-1,0,+1,0};
+    int delcol[]={0,+1,0,-1};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
         int original = image[sr][sc];
@@ -12,9 +13,9 @@ class Solution {
     private void dfs(int[][]image, int row, int col, int original, int color){
         if(row<0 || col <0 || row>=image.length || col>=image[0].length ||image[row][col]!=original)return;
         image[row][col] = color;
-        for(int[] d : dir){
-            int newRow = row + d[0];
-            int newCol = col +d[1];
+        for(int i = 0; i < 4; i++){
+            int newRow = row + delrow[i];
+            int newCol = col +delcol[i];
             if(newCol>=0 && newRow>=0 && newCol<image[0].length &&  newRow<image.length && image[newRow][newCol]==original){
                 dfs(image, newRow, newCol, original, color);
             }
