d75abeb163934f07717b6b7efb4017f479a329cc
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-20 22:47:32 +0530

    Time: 1 ms (86.8%), Space: 48.3 MB (20.66%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                bfs(isConnected, i, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+    private void bfs(int[][]isConnected, int node, boolean[]visited){
+        visited[node] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        int size = isConnected.length;
+        while(!queue.isEmpty()){
+            int city = queue.poll();
+            for(int i = 0; i < size; i++){
+                if(isConnected[city][i]==1 && !visited[i]){
+                    visited[i]=true;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
+}
