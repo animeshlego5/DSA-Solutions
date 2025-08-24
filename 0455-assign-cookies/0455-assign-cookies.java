8fbc49a8fa48380c22a96fd53570864d98eb4dec
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-24 12:09:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -22,7 +22,45 @@ class Solution {
                 greed.poll();
                 contentChildren++;
             } else {
-                // Cookie too small, try a less greedy child
+                class Solution {
+
+    public int findContentChildren(int[] greed, int[] size) {
+           int count = 0;
+           Arrays.sort(greed);
+           Arrays.sort(size);
+           int m = greed.length;
+           int n = size.length;
+
+           // greedy technique: To satisfy children with smallest possible cookie
+
+            if(m==0 || n==0) return 0;
+           // if least greedy children can't be satisfied with even largest cookie
+           // No one can't be satisfied
+
+           if(greed[0]>size[n-1])
+                return 0;
+            
+            int childPtr = 0;
+            int cookiePtr = 0;
+            
+            while(cookiePtr<n && childPtr<m)
+            {
+                if(size[cookiePtr]<greed[childPtr])
+                {
+                    // if current cookie can't satisfy current children
+                    // it can satisfy none - other children are even more greedy
+                    cookiePtr++;
+                }
+                else{
+                    // cookie size is enough to satisfy the current children
+                    cookiePtr++;
+                    childPtr++;
+                    count++;
+                }
+            }
+            return count;
+    }
+}// Cookie too small, try a less greedy child
                 greed.poll();
             }
         }
