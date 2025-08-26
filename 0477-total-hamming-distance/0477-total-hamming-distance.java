29e488eecf96d48529841f7168715391d760bda2
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-26 20:56:51 +0530

    Time: 15 ms (61.93%), Space: 45.6 MB (13.36%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.java
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -1,20 +1,19 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
-        if(nums.length==1)return 0;
-        int left = 0;
-        long ans = 0;
+
+        int ans = 0;
         int n = nums.length;
         
-        for(int i=0; i <32; i++){
-            int countOnes = 0;
+        for(int i=0; i <31; i++){
+            int y = 0;
             for(int num : nums){
                 if((num & (1<<i))!=0){
-                    countOnes++;
+                    y++;
                 }
             }
-            int countZeroes = n - countOnes;
-            ans += countZeroes * countOnes;
+            int x = n - y;
+            ans += x * y;
         } 
-    return (int)ans;
+    return ans;
     }
 }
