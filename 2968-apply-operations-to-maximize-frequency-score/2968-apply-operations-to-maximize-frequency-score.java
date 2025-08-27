05d6222f7b6aa94dba542fb498c3d5444e83f6b4
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-08-28 01:01:24 +0530

    Time: 26 ms (83.33%), Space: 60.7 MB (20.24%) - LeetHub

--- 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.java
+++ 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.java
@@ -1,63 +1,21 @@
-
-
 class Solution {
-    
-    private boolean isPossible(int length, int[] nums, long k, long[] prefixSum) {
-        
-        int leftIdx = 0, rightIdx = 0;
-        while(rightIdx < nums.length) {
-            if(rightIdx - leftIdx + 1 < length) {
-                rightIdx++;
-                continue;
-            }
-            
-            int medianIdx = leftIdx + ((rightIdx - leftIdx) / 2);
-            long curOperations = (prefixSum[rightIdx + 1] - prefixSum[medianIdx + 1]);
-            if(medianIdx > leftIdx) {
-                curOperations -= prefixSum[medianIdx] - prefixSum[leftIdx];
-            }
-            
-            if((rightIdx - leftIdx + 1) % 2 == 0) {
-                curOperations -= nums[medianIdx];
-            }
-            
-            if(curOperations <= k) {
-                return true;
-            }
-            
-            
-            leftIdx++;
-            rightIdx++;
-        }
-        
-        return false;
-    }
-    
-    public int maxFrequencyScore(int[] nums, long k) {        
-        
-        if(nums.length == 1) {
-            return 1;
-        }
-        
+    public int maxFrequencyScore(int[] nums, long k) {
         Arrays.sort(nums);
-        
-        long[] prefixSum = new long[nums.length + 1];
-        for(int idx = 0; idx < nums.length; idx++) {
-            prefixSum[idx + 1] = prefixSum[idx] + nums[idx];
-        }
-        
-        int leftFreq = 2, rightFreq = nums.length;
-        while(leftFreq <= rightFreq) {
-            int midFreq = leftFreq + ((rightFreq - leftFreq) >> 1);
-            
-            if(isPossible(midFreq, nums, k, prefixSum)) {
-                leftFreq = midFreq + 1;
-            } else {
-                rightFreq = midFreq - 1;
+        int left = 0, maxFreq = 1;
+        long cost = 0;
+        int med = 0;
+        for (int right = 1; right < nums.length; right++) {
+            cost += nums[right] - nums[med];
+            med = (left + right + 1) / 2;
+            while (cost > k) {
+                cost -= nums[med] - nums[left];
+                left++;
+                med = (left + right + 1) / 2;
             }
+
+            maxFreq = Math.max(maxFreq, right - left + 1);
         }
-        
-        return rightFreq;
-        
+
+        return maxFreq;
     }
 }
