c9212c1940edf77e2bde421f7ec3568880d3497f
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-09-08 22:22:08 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.47%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0)return 0;
+        if(n<=2)return 1;
+        int ans = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+        int prev3 = 0;
+        for(int state = 3; state<=n; state++){
+            ans = prev1 + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = ans;
+        }
+        return ans;
+    }
+}
