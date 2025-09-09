88df36143c78b8c2713bfcd4e657a614a291eaf4
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-09-09 23:20:09 +0530

    Time: 2 ms (93.61%), Space: 44.4 MB (24.66%) - LeetHub

---
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findKOr(int[] nums, int k) {
+        int[] dp = new int[32];
+        for(int num : nums){
+            int i = 0;
+            while(num > 0){
+                if((num & 1)==1){
+                    dp[i] += 1;
+                }
+                i++;
+                num = num>>1;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < 31; i++){
+            if(dp[i]>=k){
+                ans += (1<<i);
+            }
+        }
+        return ans;
+    }
+}
