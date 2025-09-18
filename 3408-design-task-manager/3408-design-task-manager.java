d100f3648d6751bfc6df34b3940c4b38ba5f1101
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-09-18 23:08:22 +0530

    Time: 286 ms (88.76%), Space: 163.3 MB (53.85%) - LeetHub

--- 3408-design-task-manager/3408-design-task-manager.java
+++ 3408-design-task-manager/3408-design-task-manager.java
@@ -1,42 +1,69 @@
 class TaskManager {
-    private PriorityQueue<int[]> pq;
-    private Map<Integer,Integer> taskPriority;
-    private Map<Integer,Integer> taskOwner;
-
+    private class Entry{
+        int userId;
+        int taskId;
+        int priority;
+        Entry(int userId,int taskId,int priority){
+            this.userId = userId;
+            this.taskId = taskId;
+            this.priority = priority;
+        }
+    }
+    private class Task{
+        int priority;
+        int userId;
+        Task(int userId, int priority){
+            this.priority = priority;
+            this.userId = userId;
+        }
+    }
+    private Map<Integer, Task> map;
+    private PriorityQueue<Entry> pq;
     public TaskManager(List<List<Integer>> tasks) {
-        pq = new PriorityQueue<>((a,b) -> {
-            if (b[0] != a[0]) return b[0] - a[0];
-            return b[1] - a[1];
-        });
-        taskPriority = new HashMap<>();
-        taskOwner = new HashMap<>();
-        for (List<Integer> t : tasks) add(t.get(0), t.get(1), t.get(2));
+        map = new HashMap<>();
+        pq = new PriorityQueue<>((a,b)->a.priority!=b.priority?b.priority-a.priority : b.taskId - a.taskId);
+        for(List<Integer> task : tasks){
+            add(task.get(0), task.get(1), task.get(2));
+        }
     }
-
+    
     public void add(int userId, int taskId, int priority) {
-        pq.add(new int[]{priority, taskId});
-        taskPriority.put(taskId, priority);
-        taskOwner.put(taskId, userId);
+        map.put(taskId, new Task(userId, priority));
+        pq.add(new Entry(userId, taskId, priority));
     }
-
+    
     public void edit(int taskId, int newPriority) {
-        pq.add(new int[]{newPriority, taskId});
-        taskPriority.put(taskId, newPriority);
+        Task t = map.get(taskId);
+        map.put(taskId, new Task(t.userId, newPriority));
+        pq.add(new Entry(t.userId, taskId, newPriority));
     }
-
+    
     public void rmv(int taskId) {
-        taskPriority.put(taskId, -1);
+        map.remove(taskId);
     }
-
+    
     public int execTop() {
-        while (!pq.isEmpty()) {
-            int[] t = pq.poll();
-            int p = t[0], id = t[1];
-            if (taskPriority.getOrDefault(id, -2) == p) {
-                taskPriority.put(id, -1);
-                return taskOwner.getOrDefault(id, -1);
+        while(!pq.isEmpty()){
+            Entry e = pq.peek();
+            Task t = map.get(e.taskId);
+            if(t == null || e.priority != t.priority || e.userId != t.userId){
+                pq.poll();
+                continue;
             }
+            pq.poll();
+            map.remove(e.taskId);
+            return e.userId;
         }
         return -1;
+        
     }
 }
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager obj = new TaskManager(tasks);
+ * obj.add(userId,taskId,priority);
+ * obj.edit(taskId,newPriority);
+ * obj.rmv(taskId);
+ * int param_4 = obj.execTop();
+ */
