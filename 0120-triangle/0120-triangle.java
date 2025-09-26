383942f14acd1e593f839aa88d2b1d30399a840b
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-09-26 19:41:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,23 +1,17 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        Integer[][] dp = new Integer[201][201];
+        int[][] dp = new int[201][201];
         return helper(triangle, 0, 0, dp);
     }
-
-    private int helper(List<List<Integer>> triangle, int rowIndex, int cRowIndex, Integer[][] dp) {
-        if (rowIndex == triangle.size()) {
-            return 0;
+    private int helper(List<List<Integer>> triangle, int row, int col, int[][] dp){
+        if(row > triangle.size() - 1)return 0;
+        int number = triangle.get(row).get(col);
+        if(dp[row][col]!= null){
+            return dp[row][col];
         }
-
-        if (dp[rowIndex][cRowIndex] != null) {
-            return dp[rowIndex][cRowIndex];
-        }
-
-        int result = triangle.get(rowIndex).get(cRowIndex) + 
-        Math.min(helper(triangle, rowIndex + 1, cRowIndex, dp), helper(triangle, rowIndex + 1, cRowIndex + 1, dp));
-
-        dp[rowIndex][cRowIndex] = result;
-
-        return dp[rowIndex][cRowIndex];
+        
+        int result =  number + Math.min(helper(triangle, row + 1, col, dp), helper(triangle, row + 1, col +1, dp));
+        dp[row][col] = result;
+        return result;
     }
 }
