500968b000fdf1ba84e185e32a7c7ee97418cf7b
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-05 15:14:48 +0530

    Time: 1 ms (69.8%), Space: 45.6 MB (68.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,37 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean[][]visited = new boolean[200][200];
         int rows = matrix.length;
         int cols = matrix[0].length;
 
+        int col0 = 1;
         for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+            if(matrix[i][0]==0)col0 = 0;
+            for(int j = 1; j < cols; j++){
                 if(matrix[i][j]==0){
-                    visited[i][j] = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;    
                 }
             }
         }
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(visited[i][j]){
-                    helper(i, j, matrix);
+        for(int i = 1; i < rows; i++){
+            for(int j = 1; j < cols; j++){
+                if(matrix[0][j] == 0 || matrix[i][0]==0){
+                    matrix[i][j] = 0;
                 }
             }
         }
-        return;
-    }
-    private void helper(int row, int col, int[][]matrix){
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(i==row || j==col){
-                    matrix[i][j]=0;
-                }
+        if(matrix[0][0]==0){
+            for(int i = 0; i < cols; i++){
+                matrix[0][i] = 0;
             }
         }
-        return;
-
+        if(col0 == 0){
+            for(int i = 0; i < rows; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
