b81e1fc9b7062d400f3fd5ca8869048b5a2d595c
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-16 16:56:45 +0530

    Time: 0 ms (100%), Space: 44.5 MB (51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode s = head, f = head;
-
-        while(f!=null && f.next!=null){
-            s = s.next;
-            f = f.next.next;
-            if(s==f){
-                return true;
-            }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow)return true;
         }
         return false;
-
     }
 }
