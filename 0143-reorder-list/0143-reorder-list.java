406cec358f264c4a3e1ef26d3cdc4a0314882d44
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-16 17:29:59 +0530

    Time: 2 ms (84.69%), Space: 48 MB (90.81%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,42 +10,33 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null || head.next.next==null){
-            return;
-        }
         ListNode slow = head;
         ListNode fast = head;
-
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        
         ListNode secondHead = slow.next;
-        slow.next= null;
-        //broke into two lists
+        slow.next = null;
 
-        //reverse the 2nd list
-        ListNode cur = secondHead;
+        //reverse this list
         ListNode prev = null;
-        while(cur!=null){
-            ListNode nextNode = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = nextNode;
+        while(secondHead!=null){
+            ListNode next = secondHead.next;
+            secondHead.next = prev;
+            prev = secondHead;
+            secondHead = next;
         }
-
-        //inserrt the l2 into l1
+        //prev is new head
         ListNode t1 = head;
         ListNode t2 = prev;
-        
+
         while(t2!=null){
             ListNode m1 = t1.next;
             ListNode m2 = t2.next;
-
             t1.next = t2;
             t2.next = m1;
-            t1=m1;
+            t1 = m1;
             t2 = m2;
         }
     }
