3d42534cd32eca31c53e15bd167e5a6541bcb3da
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-16 13:39:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (24.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,35 +10,22 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        else if (list2 == null) return list1;
-
-        ListNode head, ptr;
-
-        // Initialize head and ptr
-        if (list1.val < list2.val) {
-            head = ptr = list1;
-            list1 = list1.next;
-        } else {
-            head = ptr = list2;
-            list2 = list2.next;
-        }
-
-        // Main merge loop
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                ptr.next = list1;
-                list1 = list1.next;
-            } else {
-                ptr.next = list2;
+        if(list1 ==null)return list2;
+        if(list2==null)return list1;
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val > list2.val){
+                curr.next = list2;
                 list2 = list2.next;
+            } else {
+                curr.next = list1;
+                list1 = list1.next;
             }
-            ptr = ptr.next;
+            curr = curr.next;
         }
-
-        // Attach the remaining part of the non-empty list
-        ptr.next = (list1 != null) ? list1 : list2;
-
-        return head;
+        if(list1==null)curr.next = list2;
+        if(list2==null)curr.next = list1;
+        return dummy.next;
     }
 }
