638d0cdde280335441cc6471cd9f7092e345bad8
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-21 03:54:39 +0530

    Time: 1 ms (100%), Space: 44.2 MB (94.09%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,30 +11,20 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
-        ListNode head = null, temp = null;
-
+        ListNode dummyNode = new ListNode(0);
+        ListNode curr = dummyNode;
         while(l1!=null || l2!=null || carry!=0){
-            int val1 = (l1==null)?0:l1.val;
-            int val2 = (l2 == null)?0:l2.val;
-            
-            int value = val1 + val2 + carry;
-            ListNode ans = new ListNode(value%10);
-            carry = value / 10;
-
-            if(head==null){
-                head = ans;
-                temp = ans;
-            } else {
-                temp.next = ans;
-                temp = temp.next;
-            }
-            if(l1!=null){
-                l1 = l1.next;
-            }
-            if(l2!=null){
-                l2 = l2.next;
-            }
+            int value1 = l1==null?0:l1.val;
+            int value2 = l2==null?0:l2.val;
+            int sum = value1 + value2 + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            ListNode temp = new ListNode(sum);
+            curr.next = temp;
+            curr = curr.next;
+            l1 = l1==null?null: l1.next;
+            l2 = l2==null?null: l2.next;
         }
-        return head;
+        return dummyNode.next;
     }
 }
