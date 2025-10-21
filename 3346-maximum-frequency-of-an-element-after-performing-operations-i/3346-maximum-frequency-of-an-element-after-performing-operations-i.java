51cfed6b47df4122632378943e159cebf88f2add
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-21 20:45:41 +0530

    Time: 6 ms (96.71%), Space: 57.1 MB (82.24%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,33 +1,32 @@
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        int maxNum = 0;
+        int n = nums.length;
+        int maxnum = nums[0];
         for(int num : nums){
-            if(maxNum<num){
-                maxNum = num;
+            if(maxnum< num){
+                maxnum = num;
             }
         }
-        int[] count = new int[maxNum + 2 + k];
-        for(int num : nums){
+        int[] count = new int[maxnum + k +2];
+        for(int num: nums){
             count[num]++;
         }
-
         int[] prefix = new int[count.length];
         prefix[0] = count[0];
         for(int i = 1; i < count.length; i++){
             prefix[i] = prefix[i-1] + count[i];
         }
+
         int ans = 0;
-        for(int i = 0; i < count.length - k;i++){
-            int left = Math.max(0, i-k-1);
-            int right = Math.min(i+k, prefix.length - 1);
+        for(int i = 0; i < count.length - k; i++){
+            int left = Math.max(0, i -k -1);
+            int right = Math.min(i+k, count.length -1);
             int mid = count[i];
-
-            int convertibleAmount = prefix[right] - prefix[left] - mid;
-
-            if(convertibleAmount > numOperations){
+            int convertible = prefix[right] - prefix[left] - mid;
+            if(convertible>numOperations){
                 ans = Math.max(ans, mid + numOperations);
             } else {
-                ans = Math.max(ans, mid + convertibleAmount);
+                ans = Math.max(ans, mid + convertible);
             }
         }
         return ans;
