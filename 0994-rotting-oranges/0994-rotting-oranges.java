b491f72cd13d2bba9621ed7be25cd2fd1fdc21d7
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-23 16:53:18 +0530

    Time: 2 ms (65.69%), Space: 43.3 MB (6.54%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,42 +1,47 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0)
-            return -1;
+        int row = grid.length;
+        int col = grid[0].length;
 
-        int rows = grid.length, cols = grid[0].length;
-        int[][] time = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            Arrays.fill(time[i], Integer.MAX_VALUE);
-        }
-        int day = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
-                    dfs(grid, time, i, j, day);
+        int freshOranges = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j]==1){
+                    freshOranges++;
+                } else if(grid[i][j]==2){
+                    queue.add(new int[]{i, j});
                 }
             }
         }
-        int res = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    if (time[i][j] == Integer.MAX_VALUE)
-                        return -1;
-                    res = Math.max(res, time[i][j]);
-                }
+        if(freshOranges==0)return 0;
+
+        int time = 0;
+        while(!queue.isEmpty()){
+            
+            
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int[] current = queue.poll();
+                int r = current[0];
+                int c = current[1];
+
+                int[][] neighbours = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+                if(grid[r][c]==2){
+                    for(int[] neighbour : neighbours){
+                        int nr = r + neighbour[0];
+                        int nc = c + neighbour[1];
+                        if(nc>=0 && nr>=0 && nr < row && nc < col && grid[nr][nc]!=2 && grid[nr][nc]!=0){
+                            freshOranges--;
+                            grid[nr][nc] = 2;
+                            queue.offer(new int[]{nr, nc});
+                        }
+                        if(freshOranges==0)return ++time;
+                    }
+                }        
             }
+            time++;
         }
-        return res;
-
-    }
-
-    private void dfs(int[][] grid, int[][] time, int i, int j, int day) {
-        if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] != 0 && day<time[i][j]) {
-            time[i][j] = day;
-            dfs(grid, time, i + 1, j, day + 1);
-            dfs(grid, time, i - 1, j, day + 1);
-            dfs(grid, time, i, j + 1, day + 1);
-            dfs(grid, time, i, j - 1, day + 1);
-        }
+        return -1;
     }
 }
