3b95de8e5ee03ce6cfed2f0890f8b0e86336676c
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-26 20:45:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.java
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.java
@@ -1,38 +1,39 @@
-public class ATM {
-	int[] nominals;
-    
-    long[] counts;
-
+class ATM {
+    int[] denoms;
+    int[] bankNotes;
     public ATM() {
-        nominals = new int[]{20, 50, 100, 200, 500};
-        counts = new long[5];
+        bankNotes = new int[5];
+        denoms = new int[]{20, 50, 100, 200, 500};
     }
-
+    
     public void deposit(int[] banknotesCount) {
-        for (int i = 0; i < 5; i++)
-            counts[i] += banknotesCount[i];
+        for(int i = 0; i < 5; i++){
+            bankNotes[i] += banknotesCount[i];
+        }    
     }
-
+    
     public int[] withdraw(int amount) {
-        int[] delivery = new int[5];
-        long currentAmount = amount;
-        for (int i = 4; i >= 0; i--) {
-            if (currentAmount > nominals[i]*counts[i])
-                delivery[i] = (int) counts[i];
-            else
-                delivery[i] = (int) currentAmount/nominals[i];
-
-            currentAmount += (long) -nominals[i]*delivery[i];
-            if (currentAmount == 0)
-                break;
+        int[] ans = new int[5];
+        for(int i = 4; i >=0; i--){
+            if(amount>=denoms[i] * bankNotes[i]){
+                ans[i] = bankNotes[i];
+            } else {
+                ans[i] = amount/denoms[i];
+            }
+            amount -= denoms[i]*ans[i];
+            if(amount==0)break;
         }
-
-        if (currentAmount > 0)
-            return new int[]{-1};
-
-        for (int i = 0; i < 5; i++)
-            counts[i] += -delivery[i];
-
-        return delivery;
+        if(amount>0)return new int[]{-1};
+        for(int i = 0; i < 5; i++){
+            bankNotes[i] -= ans[i];
+        }
+        return ans;
     }
 }
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM obj = new ATM();
+ * obj.deposit(banknotesCount);
+ * int[] param_2 = obj.withdraw(amount);
+ */
