951333d28a31637b90ef90dc051b8ecc414892da
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-10-27 15:37:47 +0530

    Time: 12 ms (53.73%), Space: 45.4 MB (35.36%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -4,8 +4,8 @@ class Solution {
         int res = 0;
         for(int i = 0; i < bank.length; i++){
             int count = 0;
-            for(char c : bank[i].toCharArray()){
-                if(c=='1')count++;
+            for(int j = 0; j < bank[i].length(); j++){
+                if(bank[i].charAt(j)=='1')count++;
             }
             if(count==0)continue;
             res += prev * count;
