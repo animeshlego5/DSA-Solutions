451db22cdbd1c6417e278c382188965d5f243876
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-11-04 20:44:28 +0530

    Time: 13 ms (42.07%), Space: 47 MB (7.59%) - LeetHub

---
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
@@ -0,0 +1,49 @@
+class Solution {
+    private class freq {
+        int val;
+        int freq;
+        freq(int val, int freq){
+            this.val = val;
+            this.freq = freq;
+        }
+    }
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int i = 0; i < k; i++){
+            int num = nums[i];
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+        ans[0] = calculateSum(freq, x);
+        for(int i = k; i < n; i++){
+            int newElement = nums[i];
+            int oldElement = nums[i-k];
+            freq.put(newElement, freq.getOrDefault(newElement, 0)+1);
+            freq.put(oldElement, freq.getOrDefault(oldElement, 0)-1);
+            if(freq.getOrDefault(oldElement, 0)==0)freq.remove(oldElement);
+            ans[i-k+1] = calculateSum(freq, x);
+        }
+        
+        return ans;
+    }
+    private int calculateSum(Map<Integer, Integer>freq, int x){
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(freq.entrySet());
+
+        Collections.sort(list, (a,b)->{
+            if(!a.getValue().equals(b.getValue())){
+                return b.getValue()-a.getValue();
+            }
+            return b.getKey() - a.getKey();
+        });
+
+        int sum = 0;
+        for(Map.Entry<Integer,Integer> e:list){
+            if(x==0)break;
+            sum+= e.getKey() * e.getValue();
+            x--;
+        }
+        return sum;
+    }
+}
