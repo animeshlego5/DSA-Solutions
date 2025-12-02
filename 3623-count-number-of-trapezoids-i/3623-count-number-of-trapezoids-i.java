8a4986e8d077eda9cbbfc4f176225c1a4f7f7737
Animesh <126562373+animeshlego5@users.noreply.github.com>
2025-12-02 20:46:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countTrapezoids(int[][] points) {
+        long MOD = 1000007;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int[] p : points){
+            map.put(p[1], map.getOrDefault(p[1], 0)+1);
+        }
+
+        List<Integer> yCord = new ArrayList<>(map.keySet());
+        Collections.sort(yCord);
+
+        List<Long> hPlanes = new ArrayList<>();
+        for(int p : yCord){
+            long count = map.get(p);
+            if(count>=2){
+                hPlanes.add((count * (count-1))/2);
+            } else {
+                hPlanes.add(0L);
+            }
+        }
+        long prefix = 0;
+        long result = 0;
+        for(long l : hPlanes){
+            result = (result + prefix * (l%MOD))%MOD;
+            prefix += l&MOD;
+        }
+        return (int) result;
+    }
+}
